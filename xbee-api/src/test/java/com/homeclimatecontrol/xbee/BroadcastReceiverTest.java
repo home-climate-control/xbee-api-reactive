45bfe6a9d023791426fdc5ab03d6d48513dc6566
Vadim Tkachenko <vt@homeclimatecontrol.com>
2021-10-29 21:38:55 -0700

    Added a test case for the packet reader (#14)

---
+++ xbee-api/src/test/java/com/homeclimatecontrol/xbee/BroadcastReceiverTest.java
@@ -0,0 +1,29 @@
+package com.homeclimatecontrol.xbee;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.homeclimatecontrol.xbee.TestPortProvider.getCoordinatorTestPort;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class BroadcastReceiverTest {
+
+    private final Logger logger = LogManager.getLogger();
+
+    @Test
+    @Disabled("Enable only if safe to use hardware is connected")
+    void listen() {
+        assertThatCode(() -> {
+            try (var xbee = new XBeeReactive(getCoordinatorTestPort())) {
+                xbee
+                        .receive(null)
+                        .take(10)
+                        .doOnNext(p -> logger.info("received: {}", p))
+                        .blockLast();
+            }
+        }).doesNotThrowAnyException();
+
+    }
+}
--- xbee-api/src/test/java/com/homeclimatecontrol/xbee/TestPortProvider.java
+++ xbee-api/src/test/java/com/homeclimatecontrol/xbee/TestPortProvider.java
@@ -1,11 +1,29 @@
 package com.homeclimatecontrol.xbee;
 
 public class TestPortProvider {
+
+    /**
+     * Generic test port.
+     */
     private final static String XBEE_TEST_PORT ="XBEE_TEST_PORT";
+
+    /**
+     * Test port for tests where the coordinator firmware is expected.
+     */
+    private final static String XBEE_COORDINATOR_TEST_PORT ="XBEE_COORDINATOR_TEST_PORT";
+
     public static String getTestPort() {
-        String port = System.getenv(XBEE_TEST_PORT);
+        return getTestPort(XBEE_TEST_PORT);
+    }
+
+    public static String getCoordinatorTestPort() {
+        return getTestPort(XBEE_COORDINATOR_TEST_PORT);
+    }
+
+    public static String getTestPort(String portVariable) {
+        String port = System.getenv(portVariable);
         if (port == null) {
-            throw new IllegalStateException("Define environment variable XBEE_TEST_PORT=<your-safe-test-port> to run this test");
+            throw new IllegalStateException("Define environment variable " + portVariable + "=<your-safe-test-port> to run this test");
         }
         return port;
     }
