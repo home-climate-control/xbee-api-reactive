99c9a855ccff3b087ba9ebeb5d7b5c09e766ce85
Vadim Tkachenko <vt@homeclimatecontrol.com>
2021-11-08 18:13:32 -0700

    Bugfix: properly reading AP response

--- xbee-api/src/main/java/com/homeclimatecontrol/xbee/response/command/APResponseReader.java
+++ xbee-api/src/main/java/com/homeclimatecontrol/xbee/response/command/APResponseReader.java
@@ -1,17 +1,14 @@
 package com.homeclimatecontrol.xbee.response.command;
 
-import java.nio.ByteBuffer;
-
 /**
  * AP command response reader.
  *
  * @author Copyright &copy; <a href="mailto:vt@homeclimatecontrol.com">Vadim Tkachenko</a> 2021
  */
-public class APResponseReader extends CommandResponseReader {
+public class APResponseReader extends SingleByteResponseReader {
 
     @Override
-    public CommandResponse read(ByteBuffer commandData) {
-        // AP command requires no separate response, it is contained in the status
-        return null;
+    protected CommandResponse create(byte payload) {
+        return new APResponse(payload);
     }
 }
