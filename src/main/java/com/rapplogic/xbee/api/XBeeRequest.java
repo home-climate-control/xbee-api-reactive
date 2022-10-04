3479626bce99980a883f35ae03f83363ae9f8f5d
Vadim Tkachenko <vt@homeclimatecontrol.com>
2022-10-03 23:09:27 -0700

    Replaced CRLF with LF, no logic changes (#19)

--- src/main/java/com/rapplogic/xbee/api/XBeeRequest.java
+++ src/main/java/com/rapplogic/xbee/api/XBeeRequest.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2008 Andrew Rapp. All rights reserved.
- *  
+ *
  * This file is part of XBee-API.
- *  
+ *
  * XBee-API is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * XBee-API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with XBee-API.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,57 +29,58 @@ import java.io.Serializable;
  * @author andrew
  *
  */
-	
+
 public abstract class XBeeRequest implements Serializable {
 
-	private static final long serialVersionUID = -9181542059678009341L;
+    private static final long serialVersionUID = -9181542059678009341L;
 
-	public static final int DEFAULT_FRAME_ID = 1;
-	// XBee will not generate a TX Status Packet if this frame id sent
-	public static final int NO_RESPONSE_FRAME_ID = 0;
-	
-	private ApiId apiId;
-	private int frameId;
-	
-	public XBeeRequest() {
-	
-	}
-	
-	// TODO create XBeePacket(XBeeRequest) constructor and move operation there
-	public XBeePacket getXBeePacket() {
-		int[] frameData = this.getFrameData();
-		
-		if (frameData == null) {
-			throw new RuntimeException("frame data is null");
-		}
-		
-		// TODO xbee packet should handle api/frame id
-		XBeePacket packet = new XBeePacket(frameData);
-		
-		return packet;
-	}
+    public static final int DEFAULT_FRAME_ID = 1;
+    // XBee will not generate a TX Status Packet if this frame id sent
+    public static final int NO_RESPONSE_FRAME_ID = 0;
 
-	public abstract int[] getFrameData();
+    private ApiId apiId;
+    private int frameId;
 
-	public ApiId getApiId() {
-		return apiId;
-	}
+    public XBeeRequest() {
 
-	public int getFrameId() {
-		return frameId;
-	}
-	
-	public String toString() {
-		return "apiId=" + this.getApiId() + ",frameId=" + this.getFrameId();
-	}
+    }
 
-	public void setApiId(ApiId apiId) {
-		this.apiId = apiId;
-	}
+    // TODO create XBeePacket(XBeeRequest) constructor and move operation there
+    public XBeePacket getXBeePacket() {
+        int[] frameData = getFrameData();
 
-	public void setFrameId(int frameId) {
-		this.frameId = frameId;
-	}
-	
-	// TODO clear method to reuse request
+        if (frameData == null) {
+            throw new RuntimeException("frame data is null");
+        }
+
+        // TODO xbee packet should handle api/frame id
+        XBeePacket packet = new XBeePacket(frameData);
+
+        return packet;
+    }
+
+    public abstract int[] getFrameData();
+
+    public ApiId getApiId() {
+        return apiId;
+    }
+
+    public int getFrameId() {
+        return frameId;
+    }
+
+    @Override
+    public String toString() {
+        return "apiId=" + getApiId() + ",frameId=" + getFrameId();
+    }
+
+    public void setApiId(ApiId apiId) {
+        this.apiId = apiId;
+    }
+
+    public void setFrameId(int frameId) {
+        this.frameId = frameId;
+    }
+
+    // TODO clear method to reuse request
 }
